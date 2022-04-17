5f92c75fa38ad06ce166e1d71cb76a0d1bd38f38
Juan Sebastian Torres Parra <90060291+jstcode99@users.noreply.github.com>
2022-04-17 16:30:35 -0500

    Banner para vista de matches

---
+++ components/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">
+                {{ liga }}
+            </h1>
+            <p class="lead">List of matches by {{ liga }}</p>
+            <b-button variant="primary" to="/">Back</b-button>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent, toRef } from '@vue/composition-api'
+
+export default defineComponent({
+    props: {
+        liga: String
+    },
+    setup(props) {
+        const liga = toRef(props, 'title')
+    },
+})
+</script>
